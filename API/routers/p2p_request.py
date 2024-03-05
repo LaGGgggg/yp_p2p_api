637d3b0713e8747a306d44dd9f6d1a00770dec66
Nikita <Poni22poni23@yandex.ru>
2024-03-05 21:16:52 +0400

    feat: добавлена проверка на отсутствие активных review пользователя
    Теперь пользователь не сможет начать review, если у него уже есть активное (возвращается ErrorResponse).

--- API/routers/p2p_request.py
+++ API/routers/p2p_request.py
@@ -29,8 +29,14 @@ def create_p2p_request(
 def p2p_request_review(
         current_user: models.User = Security(login_manager, scopes=['p2p_request']),
         db: Session = Depends(get_db)
-    project = crud.P2PRequestCrud(db).start_review(current_user.id)
 ) -> schemas.P2PRequest | schemas.ErrorResponse:
+
+    p2p_request_crud = crud.P2PRequestCrud(db)
+
+    if p2p_request_crud.get(review_state=models.P2PRequest.PROGRESS, reviewer_id=current_user.id):
+        return schemas.ErrorResponse(context='You already have a review, complete it first')
+
+    project = p2p_request_crud.start_review(current_user.id)
 
     if not project:
         return schemas.ErrorResponse(context='There are not any pending projects')
